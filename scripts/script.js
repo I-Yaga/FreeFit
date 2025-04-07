4246582c4f760544316aabdf69e6f8ed9d252dd8
Ilya <yagafov08@gmail.com>
2025-04-07 19:20:58 +0400

    done

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Menu
+// modal menu
 
 function openModal() {
     document.getElementById("modal").style.top = "0px";
@@ -6,4 +6,21 @@ function openModal() {
 
 function closeModal() {
     document.getElementById("modal").style.top = "-400px";
+}
+
+// Smooth Scroll
+      
+const links = document.querySelectorAll(".hero-arrow");
+
+for (const link of links) {
+link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
 }
